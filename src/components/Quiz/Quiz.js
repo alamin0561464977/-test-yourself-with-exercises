d99b5aa1dfccbaded462cb72984f93400e1b21ec
alamin0561464977 <amin23435678901315@gmail.com>
2022-10-11 02:57:50 +0300

    added a Quiz Card

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,11 +1,19 @@
 import React, { useContext } from 'react';
 import { QuizContext } from '../../layout/Main';
+import QuizCard from '../QuizCard/QuizCard';
 
 const Quiz = () => {
     const quiz = useContext(QuizContext);
     return (
-        <div>
-            Quiz {quiz.length}
+        <div className='m-14'>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-6'>
+                {
+                    quiz.map(quiz => <QuizCard
+                        key={quiz.id}
+                        quiz={quiz}
+                    ></QuizCard>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/components/QuizCard/QuizCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const QuizCard = ({ quiz }) => {
+    const { id, name, logo } = quiz;
+    console.log(quiz)
+    return (
+        <div className=''>
+            <div className="card card-compact w-auto bg-base-100 shadow-xl">
+                <img className='h-80' src={logo} alt="Shoes" />
+                <div className="card-body">
+                    <h2 className="card-title">{name}</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <Link to={`${id}`}>
+                            <button className="btn btn-primary">Buy Now</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default QuizCard;
